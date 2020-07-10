2502b2062f7808151f0c731eb49278f03ee03294
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-07-10 19:47:53 +0600

    setting up MongoDB with mLab & Mongoose

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const mongoose = require('mongoose');
+const db = "mongodb+srv://usershahriar:passwordshahriar@users.kmmww.mongodb.net/<dbname>?retryWrites=true&w=majority" ;
+
+mongoose.connect(db, err => {
+    if(err){
+        console.error('Error!' + err)
+    }else{
+        console.log('Connected to mongodb');
+    }
+})
 
 router.get('/', (req, res)=>{
     res.send('From API route');
