6471b2b237df9da125669cdbcc8a343577e8d3eb
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-07-10 23:55:29 +0600

    implemented user registration ui

--- ngApp/src/app/register/register.component.ts
+++ ngApp/src/app/register/register.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  registerUserData = {}
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  registerUser(){
+    console.log(this.registerUserData);
   }
 
 }
