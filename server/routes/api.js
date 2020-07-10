7e71e52f0d465579fc908c02bd0d2505f63a45ca
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-07-10 21:12:24 +0600

    creating REST APIs - user login  API

--- server/routes/api.js
+++ server/routes/api.js
@@ -30,4 +30,30 @@ router.post('/register', (req, res) => {
     });
 });
 
+
+router.post('/login', (req, res) => {
+    let userData = req.body;
+
+    User.findOne({email: userData.email}, (error, user) => {
+        if(error){
+            console.log(error);
+
+        }else{
+            if(!user){
+                res.status(401).send('Invalid Email');
+
+            }else{
+                if(user.password !== userData.password){
+                    res.status(401).send('Invalid Password');
+
+                }else{
+                    res.status(200).send(user);
+                }
+
+            }
+        }
+    });
+
+} );
+
 module.exports = router;
