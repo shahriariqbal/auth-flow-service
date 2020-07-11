b15d4078b58c62592306af0f6710d6f8cbaa82a1
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-07-11 21:28:22 +0600

    fix bug

--- ngApp/src/app/app.component.ts
+++ ngApp/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { AuthService } from './auth.service';
 export class AppComponent {
   title = 'ngApp';
 
-  constructor(private _authService: AuthService){}
+  constructor(public _authService: AuthService){}
 }
