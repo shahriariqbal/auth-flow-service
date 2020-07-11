aa055ac16c9833b97c122698ff6992ad785cd4fd
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-07-11 12:47:03 +0600

    middleware to verify token

--- server/routes/api.js
+++ server/routes/api.js
@@ -14,6 +14,26 @@ mongoose.connect(db, err => {
     }
 })
 
+function verifyToken(req, res, next) {
+    if(!req.headers.authorization){
+        return res.status(401).send('Unauthorized request')
+
+    }
+    let token = req.headers.authorization.split(' ')[1]
+    if (token === 'null'){
+        return res.status(401).send('Unauthorized request')
+
+    }
+    let payload = jwt.verify(token, 'secretkey')
+    if(!payload) {
+        return res.status(401).send('Unauthorized request')
+
+    }
+    req.userId = payload.subject
+    next()
+
+}
+
 router.get('/', (req, res)=>{
     res.send('From API route');
 
@@ -104,7 +124,7 @@ router.get('/events', (req, res) => {
 
 
 
-router.get('/special', (req, res) => {
+router.get('/special', verifyToken, (req, res) => {
     let events = [
         {
             "_id": "1",
